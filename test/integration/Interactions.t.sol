c01e0fd47f5faacb9dfd177966a51e0c696ea8b8
Giovidoh <giovidoh@gmail.com>
2026-06-21 17:38:11 +0000

    feat: add Makefile, standard json input for raffle contract

---
+++ test/integration/Interactions.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+// Unit
+// integrations
+// forked
+// staging <- run tests on a mainnet or testnet
+
+// fuzzing
+// stateful fuzz
+// stateless fuzz
+// formal verification
